551fad6433b18887b21cfb8929a110ccd56b87fb
yunitarchive <fenyyunita.line@gmail.com>
2024-09-09 16:10:04 +0700

    update gettextreverse

--- src/ArrayManipulation.java
+++ src/ArrayManipulation.java
@@ -1,4 +1,11 @@
 public class ArrayManipulation {
+    /* Make method that give return true or false if the input is an array consist of numbers that have duplicate value
+    1. Store variable to determine the size of an array
+    2. Allow user to input number as they wish
+    3. Loop outer to check the value similarities to the second two last
+    4. Loop inner to check the value similarities start the second elements to last
+    5. If during iteration they match the same value then it  is found there is duplicate
+    */
     public static boolean findDuplicateInt() {
         boolean isDuplicate = false;
         int  userSize = InputUser.getInt("How many numbers do you want to store as collection number of an array? ");
@@ -16,5 +23,18 @@ public class ArrayManipulation {
             }
         }
         return isDuplicate;
+    }
+
+    public static String getTextReverse(){
+        String userText = InputUser.getString("What word would you like to reverse:? ");
+        String resUserText = "";
+        char[] userTextArr = userText.toCharArray();
+
+        //System.out.println(userText.length());
+        for(int s = userText.length() -1 ; s>=0 ; s--){
+          resUserText += userTextArr[s];
+        }
+
+        return resUserText;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void  main (String[] args){
         // No.2
         System.out.println( "We found the numbers you enter is " + ArrayManipulation.findDuplicateInt()  + " to have duplicate value");
-
+        // No.5
+        System.out.println( "Here is the reverse character, you have entered :  " + ArrayManipulation.getTextReverse());
     }
 }
